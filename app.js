f9244269245395a99d5c1c30e0314865f14e3ef5
Solin <solinda90@gmail.com>
2024-06-04 17:34:33 +0100

    nodemon

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const cors = require('cors');
 const bcryptjs = require('bcryptjs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { redisClient } = require('./utils/redisClient');
+const  redisClient  = require('./utils/redisClient');
 const config = require('./config');
 const authRoutes = require('./routes/authRoutes');
 const fileRoutes = require('./routes/fileRoutes');
 const apiKeyRoutes = require('./routes/apiKeyRoutes');
 
+
 const app = express();
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -23,12 +23,10 @@ mongoose.connect(config.dbUri)
 redisClient.on('connect', () => {
   console.log('Redis connected');
 });
-
 app.use('/auth', authRoutes);
 app.use('/files', fileRoutes);
 app.use('/api-keys', apiKeyRoutes);
 
-// Route for root URL
 app.get('/', (req, res) => {
   res.send('Welcome to the KryptoniteApp API');
 });
